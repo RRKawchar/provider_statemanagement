0234669888b2f5ba7b8b5e90570e27ac3ef590e8
RRKawchar <riyazurrohmankawchar@gmail.com>
2023-09-20 20:16:00 +0600

    value notifier listener

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,8 @@ import 'package:provider_statemanagement/provider/count_provider.dart';
 import 'package:provider_statemanagement/provider/favorite_provider.dart';
 import 'package:provider_statemanagement/provider/progressing_provider.dart';
 import 'package:provider_statemanagement/provider/theme_change_provider.dart';
-import 'package:provider_statemanagement/screens/count_example.dart';
-import 'package:provider_statemanagement/screens/favorite_screen.dart';
-import 'package:provider_statemanagement/screens/home_screen.dart';
-import 'package:provider_statemanagement/screens/progressing_screen.dart';
 import 'package:provider_statemanagement/screens/theme_screen.dart';
-import 'package:provider_statemanagement/screens/why_provider_screen.dart';
+import 'package:provider_statemanagement/screens/value_notifier_listner.dart';
 import 'package:provider_statemanagement/utils/my_theme.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -56,7 +52,7 @@ class MyApp extends StatelessWidget {
         theme: MyTheme.lightTheme,
         themeMode: themeProvider.themeMode,
         darkTheme: MyTheme.darkTheme,
-        home: const ThemeScreen(),
+        home:  ValueNotifierListener(),
       );
     },)
     );
---
+++ lib/screens/value_notifier_listner.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class ValueNotifierListener extends StatelessWidget {
+  ValueNotifierListener({super.key});
+
+  ValueNotifier<int> _counter = ValueNotifier<int>(0);
+  ValueNotifier<bool> toggle = ValueNotifier<bool>(true);
+
+  @override
+  Widget build(BuildContext context) {
+    print("Rebuild");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Notifier Listener"),
+      ),
+      body: Column(
+        children: [
+          ValueListenableBuilder(
+              valueListenable: toggle,
+              builder: (context,value,child){
+                return  TextFormField(
+                  obscureText: toggle.value,
+                  decoration: InputDecoration(
+                      hintText: "Password",
+                      suffixIcon: InkWell(
+
+                          onTap: (){
+                            toggle.value=!toggle.value;
+                          },
+                          child: Icon(toggle.value?Icons.visibility_off:Icons.visibility))
+                  ),
+                );
+              }
+          ),
+
+          Center(
+            child: ValueListenableBuilder(
+              valueListenable: _counter,
+              builder: (context,value,child){
+                return Text(
+                  _counter.value.toString(),
+                  style: TextStyle(fontSize: 50),
+                );
+              },
+            ),),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          _counter.value++;
+          print(_counter.value.toString());
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+
+  }
+
+}
